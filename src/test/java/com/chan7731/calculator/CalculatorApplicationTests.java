f1a9f8156ee579849f85173cd1586d2f97830544
박찬혁 <cksgurrhtn@naver.com>
2025-06-26 11:34:56 +0900

    commit 1

---
+++ src/test/java/com/chan7731/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan7731.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
